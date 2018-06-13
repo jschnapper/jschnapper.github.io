8866e54c7c39655840413456c7528d0467dd8d74
jschnapper <jschnap717@gmail.com>
2018-06-13 01:37:15 -0700

    loading projects

--- old_personal_site/assets/js/script.js
+++ old_personal_site/assets/js/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
 
+	// prevent layout from changing
+    $('#project-container a').each(function(project) {
+    	 $(this).delay(100 * project).fadeIn('slow');
+    });
+
 	$(".nav-item").click(function() {
 		if ($(this).hasClass("nav-inactive")) {
 			$(".nav-item").toggleClass("nav-inactive");
 			$(".nav-item").removeClass("nav-active");
 			$(this).addClass("nav-active");
 		}
- 	})
-})
+ 	});
+});
